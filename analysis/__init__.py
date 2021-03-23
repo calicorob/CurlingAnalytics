7ff1167e6da262f5975eff80c02b06dc0c454274
Robert Currie <0rc44@queensu.ca>
2021-03-23 19:17:37 -0400

    Add win percentages notebook and helper funcs

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -35,3 +35,23 @@ def get_win_pcts(classification=0):
     col = cols.pop()
     
     return df.groupby(cols)[[col]].aggregate(['count','mean'])
+
+def _get_win_pcts_matrix(classification=0,hammer=0):
+    df = get_win_pcts(classification=classification)
+    
+    
+    return df.loc(axis=0)[:,-5:5,hammer].reset_index(drop=False).pivot(index='EndNum',columns='EndSituation',values=('Win','mean'))
+
+
+def get_win_pcts_matrix(classification,hammer):
+    if classification.lower() == 'women 10 end':
+        return _get_win_pcts_matrix(classification=0,hammer=hammer)
+    elif classification.lower()  == 'men 10 end':
+        return _get_win_pcts_matrix(classification=1,hammer=hammer)
+    elif classification.lower() == 'women 8 end':
+        return _get_win_pcts_matrix(classification=2,hammer=hammer)
+    elif classification.lower() == 'men 8 end':
+        return _get_win_pcts_matrix(classification=3,hammer=hammer)
+    else:
+        raise NotImplementedError("Invalid classification passed, soz")
+        
